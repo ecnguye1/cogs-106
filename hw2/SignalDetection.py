c50e5655911d8b6bd178b9905e06a70b3ad54f72
Ethan Nguyen <123622407+ecnguye1@users.noreply.github.com>
2023-02-08 16:35:25 -0800

    Update SignalDetection.py

--- hw2/SignalDetection.py
+++ hw2/SignalDetection.py
@@ -3,15 +3,13 @@ from scipy.stats import norm
 
 class SignalDetection:
     
-    def __init__(self, hits, misses, falaseAlarms, correctRejections):
+    def __init__(self, hits, misses, falseAlarms, correctRejections):
         self.hits = hits
         self.misses = misses
-        self.fa = falaseAlarms
+        self.fa = falseAlarms
         self.cr = correctRejections
-        
         #calculating H-hit rate
         self.h_rate = self.hits / (self.hits + self.misses)
-        
         #calculating FA false alarm rate
         self.f_rate = self.fa / (self.fa + self.cr)
         
@@ -26,5 +24,4 @@ class SignalDetection:
         z_fa = norm.ppf(self.f_rate)
         #calculating base on C = -0.5 * (Z(H) + Z(FA))
         criterion = -0.5 * (z_h  + z_fa)
-        
         return criterion
